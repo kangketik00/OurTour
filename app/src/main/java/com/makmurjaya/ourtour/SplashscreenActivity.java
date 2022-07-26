7f0459245ae8522468e835deca9f9e584e3a5979
Hayin Ananta <hayin.10119005@mahasiswa.unikom.ac.id>
2022-07-26 17:51:53 +0700

    Membuat Splash Screen

---
+++ app/src/main/java/com/makmurjaya/ourtour/SplashscreenActivity.java
@@ -0,0 +1,26 @@
+package com.makmurjaya.ourtour;
+// Tanggal Pengerjaan: 26-07-2022
+// Hayin Ananta - 10119005 - IF1
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
